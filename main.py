74341edd4e07da2ad9b9ef27bc1877e4094e5bbb
PawWitGit <git.pawelwitala@gmail.com>
2022-12-14 18:14:01 +0100

    update commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from typing import Optional
+from urllib import request
 
 import databases
 import enum
@@ -7,10 +8,12 @@ import enum
 import jwt
 import sqlalchemy
 from pydantic import BaseModel, validator
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from decouple import config
 from email_validator import validate_email as validate_e, EmailNotValidError
 from passlib.context import CryptContext
+from starlette.requests import Request
 
 DATABASE_URL = (
     f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/clothes"
@@ -19,6 +22,13 @@ DATABASE_URL = (
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
+
+
+class UserRole(enum.Enum):
+    super_admin = "super admin"
+    admin = "admin"
+    user = "user"
+
 
 users = sqlalchemy.Table(
     "users",
@@ -40,6 +50,12 @@ users = sqlalchemy.Table(
         nullable=False,
         server_default=sqlalchemy.func.now(),
         onupdate=sqlalchemy.func.now(),
+    ),
+    sqlalchemy.Column(
+        "role",
+        sqlalchemy.Enum(UserRole),
+        nullable=False,
+        server_default=UserRole.user.name,
     ),
 )
 
@@ -125,6 +141,36 @@ app = FastAPI()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
+class CustomHTTPBearer(HTTPBearer):
+    async def __call__(
+        self, request: Request
+    ) -> Optional[HTTPAuthorizationCredentials]:
+        res = await super().__call__(request)
+
+        try:
+            payload = jwt.decode(
+                res.credentials, config("JWT_SECRET"), algorithm=["HS256"]
+            )
+            user = await database.fetch_one(
+                users.select().where(users.c.id == payload["sub"])
+            )
+            request.state.user = user
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(401, "Token is expired")
+        except jwt.InvalidTokenError:
+            raise HTTPException(401, "Invalid Token")
+
+
+oauth2_scheme = CustomHTTPBearer()
+
+
+def is_admin(request: Request):
+    user = request.state.user
+    if not user and user["role"] not in (UserRole.admin, UserRole.super_admin):
+        raise HTTPException(403, "You do not have permissions for this resource")
+
+
 def create_acces_token(user):
     try:
         payload = {
@@ -146,8 +192,42 @@ async def shutdown():
     await database.disconnect()
 
 
+@app.get("/clothes/", dependencies=[Depends(oauth2_scheme)])
+async def get_all_clothes(request: Request):
+    a = 5
+    return await database.fetch_all(clothes.select())
+
+
+class ClothesBase(BaseModel):
+    name: str
+    color: str
+    size: SizeEnum
+    color: ColorEnum
+
+
+class ClothesIn(ClothesBase):
+    pass
+
+
+class ClothesOut(ClothesBase):
+    id: int
+    created_at: datetime
+    last_modified_at: datetime
+
+
+@app.post(
+    "/clothes/",
+    response_model=ClothesOut,
+    dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
+    status_code=201,
+)
+async def create_clothes(clothes_data: ClothesIn):
+    id_ = await database.execute(clothes.insert().values(**clothes_data.dict()))
+    return await database.fetch_all(clothes.select().where(clothes.c.id == id_))
+
+
 # @app.post("/register/", response_model=UserSignOut)
-@app.post("/register/")
+@app.post("/register/", status_code=201)
 async def create_user(user: UserSignIn):
     user.password = pwd_context.hash(user.password)
     q = users.insert().values(**user.dict())
