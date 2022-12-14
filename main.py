a2db5fb5eb14b06ff7aa790669254536299e3a63
PawWitGit <git.pawelwitala@gmail.com>
2022-12-14 18:21:44 +0100

    App register and clothes works properly // finish JWT // Course 5.1

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from datetime import datetime, timedelta
 from typing import Optional
-from urllib import request
 
 import databases
 import enum
 
 import jwt
 import sqlalchemy
+from h11._abnf import status_code
 from pydantic import BaseModel, validator
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, dependencies, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from decouple import config
 from email_validator import validate_email as validate_e, EmailNotValidError
 from passlib.context import CryptContext
 from starlette.requests import Request
 
-DATABASE_URL = (
-    f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/clothes"
-)
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/clothes"
 
 database = databases.Database(DATABASE_URL)
 
@@ -38,12 +36,7 @@ users = sqlalchemy.Table(
     sqlalchemy.Column("password", sqlalchemy.String(255)),
     sqlalchemy.Column("full_name", sqlalchemy.String(200)),
     sqlalchemy.Column("phone", sqlalchemy.String(13)),
-    sqlalchemy.Column(
-        "created_at",
-        sqlalchemy.DateTime,
-        nullable=False,
-        server_default=sqlalchemy.func.now(),
-    ),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()),
     sqlalchemy.Column(
         "last_modified_at",
         sqlalchemy.DateTime,
@@ -51,12 +44,7 @@ users = sqlalchemy.Table(
         server_default=sqlalchemy.func.now(),
         onupdate=sqlalchemy.func.now(),
     ),
-    sqlalchemy.Column(
-        "role",
-        sqlalchemy.Enum(UserRole),
-        nullable=False,
-        server_default=UserRole.user.name,
-    ),
+    sqlalchemy.Column("role", sqlalchemy.Enum(UserRole), nullable=False, server_default=UserRole.user.name)
 )
 
 
@@ -84,12 +72,7 @@ clothes = sqlalchemy.Table(
     sqlalchemy.Column("color", sqlalchemy.Enum(ColorEnum), nullable=False),
     sqlalchemy.Column("size", sqlalchemy.Enum(SizeEnum), nullable=False),
     sqlalchemy.Column("photo_url", sqlalchemy.String(255)),
-    sqlalchemy.Column(
-        "created_at",
-        sqlalchemy.DateTime,
-        nullable=False,
-        server_default=sqlalchemy.func.now(),
-    ),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()),
     sqlalchemy.Column(
         "last_modified_at",
         sqlalchemy.DateTime,
@@ -124,7 +107,7 @@ class BaseUser(BaseModel):
             first_name, last_name = v.split()
             return v
         except Exception:
-            raise ValueError("You should provide at least 2 names ")
+            raise ValueError("You should provide at least 2 names")
 
 
 class UserSignIn(BaseUser):
@@ -143,40 +126,34 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class CustomHTTPBearer(HTTPBearer):
     async def __call__(
-        self, request: Request
+            self, request: Request
     ) -> Optional[HTTPAuthorizationCredentials]:
         res = await super().__call__(request)
 
         try:
-            payload = jwt.decode(
-                res.credentials, config("JWT_SECRET"), algorithm=["HS256"]
-            )
-            user = await database.fetch_one(
-                users.select().where(users.c.id == payload["sub"])
-            )
+            payload = jwt.decode(res.credentials, config("JWT_SECRET"), algorithms=["HS256"])
+            user = await database.fetch_one(users.select().where(users.c.id == payload["sub"]))
             request.state.user = user
             return payload
         except jwt.ExpiredSignatureError:
             raise HTTPException(401, "Token is expired")
         except jwt.InvalidTokenError:
-            raise HTTPException(401, "Invalid Token")
+            raise HTTPException(401, "Invalid token")
 
 
 oauth2_scheme = CustomHTTPBearer()
 
 
 def is_admin(request: Request):
+
     user = request.state.user
-    if not user and user["role"] not in (UserRole.admin, UserRole.super_admin):
+    if not user or user["role"] not in (UserRole.admin, UserRole.super_admin):
         raise HTTPException(403, "You do not have permissions for this resource")
 
 
-def create_acces_token(user):
+def create_access_token(user):
     try:
-        payload = {
-            "sub": user["id"],
-            "exp": datetime.utcnow() + timedelta(minutes=120),
-        }
+        payload = {"sub": user["id"], "exp": datetime.utcnow() + timedelta(minutes=120)}
         return jwt.encode(payload, config("JWT_SECRET"), algorithm="HS256")
     except Exception as ex:
         raise ex
@@ -194,7 +171,7 @@ async def shutdown():
 
 @app.get("/clothes/", dependencies=[Depends(oauth2_scheme)])
 async def get_all_clothes(request: Request):
-    a = 5
+    user = request.state.user
     return await database.fetch_all(clothes.select())
 
 
@@ -215,23 +192,23 @@ class ClothesOut(ClothesBase):
     last_modified_at: datetime
 
 
-@app.post(
-    "/clothes/",
-    response_model=ClothesOut,
-    dependencies=[Depends(oauth2_scheme), Depends(is_admin)],
-    status_code=201,
-)
+
+@app.post("/clothes/",
+          response_model=ClothesOut,
+          dependencies=[Depends(oauth2_scheme),
+                        Depends(is_admin)],
+          status_code=201
+          )
 async def create_clothes(clothes_data: ClothesIn):
     id_ = await database.execute(clothes.insert().values(**clothes_data.dict()))
-    return await database.fetch_all(clothes.select().where(clothes.c.id == id_))
+    return await database.fetch_one(clothes.select().where(clothes.c.id == id_))
 
 
-# @app.post("/register/", response_model=UserSignOut)
-@app.post("/register/", status_code=201)
+@app.post("/register/",  status_code=201)
 async def create_user(user: UserSignIn):
     user.password = pwd_context.hash(user.password)
     q = users.insert().values(**user.dict())
     id_ = await database.execute(q)
     created_user = await database.fetch_one(users.select().where(users.c.id == id_))
-    token = create_acces_token(created_user)
+    token = create_access_token(created_user)
     return {"token": token}
